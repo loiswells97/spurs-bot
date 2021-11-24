17ba9397dee8942e592288bafe0865e09b335da0
Lois Wells <loiswells@loiswells-mbp-m1.home>
2021-11-24 17:07:20 +0000

    Print message about match every day at 10am

--- notifier.py
+++ notifier.py
@@ -2,35 +2,52 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import re
 import requests
+import schedule
+import time
 
-request=requests.get("https://www.bbc.co.uk/sport/football/teams/tottenham-hotspur/scores-fixtures")
-soup=BeautifulSoup(request.content, "html.parser")
+def generate_match_alert():
 
-next_match=soup.find("div", class_="qa-match-block")
+    request=requests.get("https://www.bbc.co.uk/sport/football/teams/tottenham-hotspur/scores-fixtures")
+    soup=BeautifulSoup(request.content, "html.parser")
 
-# print(next_match)
+    next_match=soup.find("div", class_="qa-match-block")
 
-date_string=next_match.find_all("h3")[0].string
-competition=next_match.find_all("h3")[1].string
+    # print(next_match)
 
-print(date_string)
-print(competition)
+    date_string=next_match.find_all("h3")[0].string
+    competition=next_match.find_all("h3")[1].string
 
-time_string=next_match.find("span", class_="sp-c-fixture__number--time").string
-teams=next_match.find_all("span", class_="qa-full-team-name")
-today=datetime.now()
+    print(date_string)
+    print(competition)
 
-modified_date_string=re.sub(r'(\d)(st|nd|rd|th)', r'\1', date_string)
-print(modified_date_string)
+    time_string=next_match.find("span", class_="sp-c-fixture__number--time").string
+    teams=next_match.find_all("span", class_="qa-full-team-name")
+    today=datetime.now()
 
-match_datetime=datetime.strptime(modified_date_string+" "+str(today.year)+ " "+time_string, "%A %d %B %Y %H:%M")
+    modified_date_string=re.sub(r'(\d)(st|nd|rd|th)', r'\1', date_string)
+    print(modified_date_string)
 
-if teams[0].string=="Tottenham Hotspur":
-    is_home=True
-    opposition=teams[1].string
-else:
-    is_home=False
-    opposition=teams[0].string
+    match_datetime=datetime.strptime(modified_date_string+" "+str(today.year)+ " "+time_string, "%A %d %B %Y %H:%M")
 
-print(opposition)
-print(is_home)
+    if teams[0].string=="Tottenham Hotspur":
+        home_or_away="at home"
+        opposition=teams[1].string
+    else:
+        home_or_away="away"
+        opposition=teams[0].string
+
+    print(opposition)
+    print(home_or_away)
+
+    is_today=(match_datetime.date()==today.date())
+    print(is_today)
+
+    if is_today:
+        message=f"Tottenham are playing {opposition} {home_or_away} today at {time_string}."
+        print(message)
+
+schedule.every().day.at("10:00").do(generate_match_alert)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
