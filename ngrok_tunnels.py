383df5588c9df5a06f2a9d4b27cd479ce83e93a4
Lois Wells <loiswells@icloud.com>
2025-01-08 11:09:39 +0000

    modify ngrok tunnel time

--- ngrok_tunnels.py
+++ ngrok_tunnels.py
@@ -12,5 +12,5 @@ def update_ngrok_urls():
         url = http_tunnel.public_url
         incoming_phone_number = client.incoming_phone_numbers(config.sid).update(sms_url=f"{url}/sms")
 
-        time.sleep(7.9*60*60)
+        time.sleep(7.9*60)
         ngrok.disconnect(http_tunnel.public_url)
